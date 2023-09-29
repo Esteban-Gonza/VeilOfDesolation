4f1bd793dba6032b9b3ecb87c3ebb6e36e9c8e56
Juan Esteban Gonzalez Sanchez <juanesgs2002@gmail.com>
2023-09-29 15:31:46 -0500

    Enemy-Player Interaction coded & implemented

--- Assets/Code/Scripts/Player/Scripts/PlayerMovement.cs
+++ Assets/Code/Scripts/Player/Scripts/PlayerMovement.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(AudioSource))]
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
 
     [Header("Character")]
     [SerializeField] private Animator animator;
@@ -21,15 +20,17 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D body;
     private AudioSource _audio;
+    private EnemyMovement enemy;
 
     private bool isGrounded;
     private int xScale;
     private bool isAudioPlaying;
 
-    private void Start()
-    {
+    private void Start(){
+
         body = GetComponent<Rigidbody2D>();
         _audio = GetComponent<AudioSource>();
+        enemy = FindObjectOfType<EnemyMovement>();
         _audio.clip = walkClip;
 
         isGrounded = true;
@@ -40,19 +41,16 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
-    private void Move()
-    {
+    private void Move(){
         Walk();
         Jump();
         Ascend();
     }
 
-    private void Jump()
-    {
+    private void Jump(){
         bool jumpInput = Input.GetKeyDown(KeyCode.W);
 
-        if (jumpInput && isGrounded)
-        {
+        if (jumpInput && isGrounded){
             animator.SetTrigger("Jump");
             body.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             _audio.PlayOneShot(jumpClip, 0.5f);
@@ -61,61 +59,58 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Walk()
-    {
+    private void Walk(){
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        if (Mathf.Abs(horizontalInput) > 0)
-        {
+        if (Mathf.Abs(horizontalInput) > 0){
             animator.SetFloat("Speed", 1);
 
-            if (!isAudioPlaying)
-            {
+            if (!isAudioPlaying){
                 StartCoroutine(PlayClip(walkClip, 0.1f));
             }
-        }
-        else
-        {
+        }else{
             animator.SetFloat("Speed", 0);
         }
 
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 
-        if (horizontalInput > 0)
-        {
+        if (horizontalInput > 0){
             xScale = 1;
-        }
-        else if (horizontalInput < 0)
-        {
+        }else if (horizontalInput < 0){
             xScale = -1;
         }
 
         transform.localScale = new Vector3(xScale, 1, 1);
     }
 
-    private void Ascend()
-    {
-        if (ascend)
-        {
+    private void Ascend(){
+        if (ascend){
             animator.SetBool("Ascend", true);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            if (!isGrounded)
-            {
+    private void Death(){
+        GameManager.instance.state = GameState.gameOver;
+        enemy.rotationSpeed = 0;
+        enemy.moveSpeed = 0;
+        this.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground")){
+            if (!isGrounded){
                 _audio.PlayOneShot(fallClip, 0.5f);
             }
             isGrounded = true;
             animator.SetBool("Grounded", isGrounded);
         }
+
+        if (collision.gameObject.CompareTag("Enemy")){
+            Death();
+        }
     }
 
-    private IEnumerator PlayClip(AudioClip clip, float volume)
-    {
+    private IEnumerator PlayClip(AudioClip clip, float volume){
         isAudioPlaying = true;
         _audio.PlayOneShot(clip, volume);
         yield return new WaitForSeconds(clip.length);
