7e0e305bda9335a94c863432283bff245b4cb8b1
Juan Esteban Gonzalez Sanchez <juanesgs2002@gmail.com>
2023-10-01 21:14:01 -0500

    Enemy fixed, speed changes when hit by torches, camera shake

--- Assets/Code/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Code/Scripts/Enemy/EnemyMovement.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class EnemyMovement : MonoBehaviour{
 
     public float rotationSpeed;
     public float moveSpeed;
 
+    [SerializeField] float shakeIntesity;
+    [SerializeField] float shakeTime;
+    private float shakeTimer;
+
     private Vector2 direction;
     [SerializeField] private GameObject targetPlayer;
+
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
 
     private void Update(){
 
@@ -19,5 +26,31 @@ public class EnemyMovement : MonoBehaviour{
 
         Vector2 targetPos = targetPlayer.transform.position;
         transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if(shakeTimer > 0){
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f){
+                CinemachineBasicMultiChannelPerlin basicMultiChanelPerlin =
+                    virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                basicMultiChanelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Torch")){
+            StartCoroutine(HittedByTorch());
+        }
+    }
+
+    IEnumerator HittedByTorch(){
+        moveSpeed = 0;
+        CinemachineBasicMultiChannelPerlin basicMultiChanelPerlin = 
+            virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        basicMultiChanelPerlin.m_AmplitudeGain = shakeIntesity;
+        shakeTimer = shakeTime;
+
+        yield return new WaitForSeconds(2f);
+        moveSpeed = 5;
     }
 }
